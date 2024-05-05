00458aaf9af0d522eeaef8c2f7124acb6d5dc69f
FLORIND\FlorindLatifi <florind.latifi@rrota.com>
2024-05-05 23:13:29 +0200

    added assets: fonts, scss, images, icons ,-router , and snapshot test

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+    <div>
+      hello
+    </div>
+  </Router>
+  );
+}
+
+export default App;
